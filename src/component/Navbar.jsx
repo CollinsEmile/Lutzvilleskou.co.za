0e9f0068eae86a07d5da26530a8986707f85eaf0
Emile Collins <collinsemile@gmail.com>
2025-05-22 20:44:06 +0200

    Added updates and finalized the first draft

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,41 +3,59 @@ import tiktok from '../images/TIKTOK.png';
 import instagram from '../images/INSTA.png';
 import hamburger from '../images/hamburger.svg';
 
-// function handleClick() {
-//   for (let i = 0; i < navLinks.length; i++) {
-//     navLinks[i].classList.toggle('expanded');
-//   }
-// }
-
 const Navbar = ({ logo, items }) => {
   return (
     <>
-      <div className="flex flew-row justify-between px-[10%] md:items-center py-5 bg-gray-50 text-gray-900 fixed w-full border-b-2 border-gray-900 font-georgia font-medium z-50">
+      <div className="flex flex-row justify-between px-[10%] md:items-center py-5 bg-gray-50 text-gray-900 fixed w-full border-b-2 border-gray-900 font-georgia font-medium z-50">
         {/* Logo */}
         <a href="/">
           <img src={logo} alt="Lutzville Landbouskou" className="h-[5rem]" />
         </a>
+
         {/* Navbar Links */}
-        <div className="nav-links absolute h-[60vh] md:h-[50vh] w-[100vw] left-0 top-[7rem] bg-gray-50 pt-10 lg:flex lg:flew-row lg:h-fit lg:relative lg:bg-transparent lg:w-fit lg:top-0 gap-x-5 lg:pt-0 lg:items-center border-b-2 border-gray-900 lg:border-0">
-          <ul className="flex flex-col lg:flex-row h-[80%] w-[100vw] lg:w-fit text-center">
+        <div className="nav-links absolute min-h-fit w-[100vw] left-0 top-[7rem] bg-gray-50 pt-10 lg:flex lg:flex-row lg:relative lg:bg-transparent lg:w-fit lg:top-0 gap-x-5 lg:pt-0 lg:items-center border-b-2 border-gray-900 lg:border-0">
+          <ul className="flex flex-col lg:flex-row w-[100vw] lg:w-fit text-center pb-10 lg:pb-0">
             {items.map((item) => (
               <li
                 key={item.key}
-                className="w-fit mx-auto hover:underline text-2xl lg:text-xl py-5 md:p-2">
+                className="w-fit mx-auto hover:underline text-2xl lg:text-xl py-5 lg:py-2 lg:px-2">
                 <a href={item.link}>{item.name}</a>
               </li>
             ))}
           </ul>
+
+          {/* Social Links - moved inside the nav-links div for mobile */}
+          <div className="flex flex-row gap-x-5 justify-center items-center pb-10 lg:hidden">
+            <a
+              href="https://www.facebook.com/share/VSo4BrR9fNgdKRPf/?mibextid=qi2Omg"
+              target="_blank">
+              <img
+                src={facebook.src}
+                alt="Link to facebook account"
+                className="h-[2rem]"
+              />
+            </a>
+            <a
+              href="https://www.instagram.com/lutzville_landbouskou/"
+              target="_blank">
+              <img
+                src={instagram.src}
+                alt="Link to instagram account"
+                className="h-[2rem]"
+              />
+            </a>
+            <a href="https://www.tiktok.com/@lutzvilleskou" target="_blank">
+              <img
+                src={tiktok.src}
+                alt="Link to tiktok account"
+                className="h-[2rem]"
+              />
+            </a>
+          </div>
         </div>
-        {/* Social Links */}
-        <div className="nav-links absolute h-fit left-0 top-[40rem] md:top-[30rem] w-[100vw] lg:w-fit lg:relative lg:flex lg:flew-row lg:top-0 gap-x-5 justify-center items-center">
-          {/* <a href="">
-            <img
-              src={instagram.src}
-              alt="Link to instagram account"
-              className="h-[2rem]"
-            />
-          </a> */}
+
+        {/* Social Links - separate for desktop */}
+        <div className="hidden lg:flex flex-row gap-x-5 justify-center items-center">
           <a
             href="https://www.facebook.com/share/VSo4BrR9fNgdKRPf/?mibextid=qi2Omg"
             target="_blank">
@@ -64,13 +82,11 @@ const Navbar = ({ logo, items }) => {
             />
           </a>
         </div>
+
         <button
           onClick={() => {
-            const navLinks = document.querySelectorAll('.nav-links');
-            // for (let index = 0; index < navLinks.length; index++) {
-            //   navLinks[index].classList.toggle('expanded');
-            navLinks[0].classList.toggle('expanded');
-            navLinks[1].classList.toggle('expanded');
+            const navLinks = document.querySelector('.nav-links');
+            navLinks.classList.toggle('expanded');
           }}
           className="lg:hidden">
           <img src={hamburger.src} alt="menu icon" className="h-7" />
@@ -79,4 +95,5 @@ const Navbar = ({ logo, items }) => {
     </>
   );
 };
+
 export default Navbar;
