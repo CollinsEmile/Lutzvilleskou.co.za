8fa6e203550929661be06323f9b60eb319b481ea
Emile Collins <collinsemile@gmail.com>
2025-05-29 01:41:07 +0200

    Compressed Images and fixed links and overlay

--- src/component/ACTIVITIES.js
+++ src/component/ACTIVITIES.js
@@ -5,7 +5,7 @@ const activities = [
     title: 'Potjiekos en Braaikompetisie',
     // description:
     //   'Kom deel in ons opwindende Potjiekos & Braaikompetisie waar deelnemers hul vaardighede vertoon in die kuns van tradisionele Suid-Afrikaanse kookkuns.',
-    image: '/potjiekos-cv_1200.jpg',
+    image: '/potjiekos-cv_1200.webp',
     buttonText: 'Skryf In',
     buttonLink: '/Potjiekosvorm 2025.pdf',
   },
@@ -14,7 +14,7 @@ const activities = [
     title: 'Gholf',
     // description:
     //   'Paddle through crystal clear waters and explore hidden coves and beaches.',
-    image: '/Gholf.jpg',
+    image: '/Gholf.webp',
     buttonText: 'Skryf In',
     buttonLink: '/Lutzville Landbouskou Gholfdag.pdf',
   },
@@ -23,7 +23,7 @@ const activities = [
     title: 'Perdeskou',
     // description:
     //   'Taste authentic local dishes and learn about culinary traditions from experienced chefs.',
-    image: '/www.lizellelotter.co.zaLutzvilleSkou2024-110.jpg',
+    image: '/www.lizellelotter.co.zaLutzvilleSkou2024-110.webp',
     buttonText: 'Skryf In',
     buttonLink: '/INSKRYWINGSVORM perde 2025.pdf',
   },
@@ -32,7 +32,7 @@ const activities = [
     title: 'Veteraan Trekkers en Karre',
     // description:
     //   'Rejuvenate your mind and body with our peaceful yoga sessions in nature.',
-    image: '/Veteraan trekkers en karre.jpg',
+    image: '/Veteraan trekkers en karre.webp',
     // buttonText: 'Skryf In',
     // buttonLink: '/activities/yoga',
   },
@@ -41,7 +41,7 @@ const activities = [
     title: 'Petting Zoo',
     // description:
     //   'Learn to capture stunning landscapes with professional photography instructors.',
-    image: '/www.lizellelotter.co.zaLutzvilleSkou2024-24.jpg',
+    image: '/www.lizellelotter.co.zaLutzvilleSkou2024-24.webp',
     // buttonText: 'Skryf In',
     // buttonLink: '/activities/photography',
   },
@@ -50,7 +50,7 @@ const activities = [
     title: 'Trekker Ry Kompetisie',
     // description:
     //   'Observe the night sky with telescopes and guidance from astronomy experts.',
-    image: '/TrekkerRyKompetisie.jpg',
+    image: '/TrekkerRyKompetisie.webp',
     // buttonText: 'Skryf In',
     // buttonLink: '/activities/stargazing',
   },
@@ -59,32 +59,32 @@ const activities = [
     title: 'Junior Mnr. en Mej. Skoukoning en -koningin',
     // description:
     //   'Observe the night sky with telescopes and guidance from astronomy experts.',
-    image: '/Junior.jpg',
+    image: '/Junior.webp',
     overlayText:
-      'Aangebied deur Hansie en Grietjie Kleuterskool by die Lutzville Landbouskou. <br> Kleuters van Gr. RRR tot Gr. 3 is welkom om deel te neem. <br> Datum: 31 Julie 2025 |  Tyd: 17:30 <br> Plek: Skougronde, Lutzville <br> Inskrywingsfooi: R50 per deelnemer <br> Inskrywings sluit 26 Julie 2025. <br> Toegang: R20 per toeskouer (by die hek of vooraf betaalbaar) <br> Inskrywings kan gedoen word by Hansie en Grietjie Kleuterskool of via WhatsApp. <br> Kontak Leanne Joubert vir navrae: 082 925 3994',
+      '<strong>Aangebied deur</strong> Hansie en Grietjie Kleuterskool by die Lutzville Landbouskou. <br> Kleuters van Gr. RRR tot Gr. 3 is welkom om deel te neem. <br> <strong>Datum:</strong> 31 Julie 2025 |  Tyd: 17:30 <br> <strong>Plek:</strong> Skougronde, Lutzville <br> <strong>Inskrywingsfooi:</strong> R50 per deelnemer <br> <i>Inskrywings sluit 26 Julie 2025.</i> <br> <strong>Toegang:</strong> R20 per toeskouer<br> <i>(by die hek of vooraf betaalbaar)</i> <br> Inskrywings kan gedoen word by Hansie en Grietjie Kleuterskool of via WhatsApp. <br> <strong>Navrae:</strong> Leanne Joubert - 082 925 3994',
   },
   {
     id: 8,
     title: 'Mej Skoukoningin',
     // description:
     //   'Observe the night sky with telescopes and guidance from astronomy experts.',
-    image: '/Mej Skoukoningin.jpg',
+    image: '/Mej Skoukoningin.webp',
     overlayText:
       "Stuur 'n foto of twee met 'n kort beskrywing van jouself en waar jy vandaan kom na admin@lutzvilleskou.co.za of WhatsApp na 084 580 2562.",
   },
   {
     id: 9,
     title: 'Vroueoggend',
-    image: '/Vrouedag.jpg',
+    image: '/Vrouedag.webp',
     overlayText:
-      'Kaartjies: Mia - 076 646 6512 <br> Tyd: 9h30 vir 10h00 <br> Koste: R150 per persoon',
+      '<strong>Kaartjies:</strong> Mia - 076 646 6512 <br> <strong>Tyd:</strong> 9h30 vir 10h00 <br> <strong>Koste:</strong> R150 per persoon',
   },
   {
     id: 10,
     title: 'Sweepslaan Kompetisie',
     // description:
     //   'Observe the night sky with telescopes and guidance from astronomy experts.',
-    image: '/sweep.jpg',
+    image: '/sweep.webp',
     // buttonText: 'Skryf In',
     // buttonLink: '/activities/stargazing',
   },
@@ -93,7 +93,7 @@ const activities = [
     title: 'Meganiese Bul',
     // description:
     //   'Observe the night sky with telescopes and guidance from astronomy experts.',
-    image: '/Bul.png',
+    image: '/Bul.webp',
     // buttonText: 'Skryf In',
     // buttonLink: '/activities/stargazing',
   },
@@ -102,7 +102,7 @@ const activities = [
     title: 'Sterkmankompetisie',
     // description:
     //   'Observe the night sky with telescopes and guidance from astronomy experts.',
-    image: '/Strongman 1.jpg',
+    image: '/Strongman 1.webp',
     // buttonText: 'Skryf In',
     // buttonLink: '/activities/stargazing',
   },
